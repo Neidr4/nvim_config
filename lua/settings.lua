565963432056f2a55b83de2acb3c594f86c8b3e1
Neidr4 <39047097+Neidr4@users.noreply.github.com>
2022-11-08 23:28:34 +0100

    Create settings.lua

---
+++ lua/settings.lua
@@ -0,0 +1,19 @@
+-- Basics
+vim.wo.number = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- Colorscheme Stuff
+vim.o.background = 'dark'
+vim.cmd('colorscheme gruvbox')
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
