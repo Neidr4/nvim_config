2fd99e377f8630b44dae3821e4388a3f264053b3
ADB <a.drapbiz@gmail.com>
2024-10-01 08:17:06 +0200

    feat: Remove everything. Add lazyvim version

--- after/plugin/undotree.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
--- vim.keymap.set("n", "<C-w>h", vim.cmd.TmuxNavigateLeft)
--- vim.keymap.set("n", "<C-w>l", vim.cmd.TmuxNavigateRight)
--- vim.keymap.set("n", "<C-w>j", vim.cmd.TmuxNavigateDown)
--- vim.keymap.set("n", "<C-w>k", vim.cmd.TmuxNavigateUp)
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  -- disable bufferline
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
+}
